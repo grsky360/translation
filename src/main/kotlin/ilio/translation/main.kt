a422e0b297bb106d3bdb234c1b39d7fbeb714a5b
chenscl <chenscl@ziroom.com>
2021-03-25 23:45:58 +0800

    u

--- src/main/kotlin/ilio/translation/main.kt
+++ src/main/kotlin/ilio/translation/main.kt
@@ -1,7 +1,14 @@
 package ilio.translation
 
 import androidx.compose.desktop.*
-import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.plus
+import androidx.compose.ui.input.key.shortcuts
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.window.Notifier
@@ -29,6 +36,7 @@ fun setVisible(visible: Boolean) {
 fun main() = Window(
     size = IntSize(300, 450),
     location = IntOffset.Zero,
+    centered = false,
     undecorated = true,
     resizable = false,
 ) {
@@ -40,13 +48,21 @@ fun main() = Window(
 
     SystemTray.getSystemTray().add(TrayIcon(getTrayIcon(), "tooltip").apply {
         addMouseListener(object : MouseAdapter() {
-            override fun mouseClicked(e: MouseEvent?) {
-                notifier.notify("aa", "bb")
-                setVisible(true)
-                getContext().window.requestFocus()
+            override fun mousePressed(e: MouseEvent?) {
+                if (e != null) {
+                    getContext().window.setLocation(e.x, e.y)
+                    setVisible(true)
+                }
             }
         })
     })
+
+    MaterialTheme {
+        TextField(
+            value = "",
+            onValueChange = { }
+        )
+    }
 }
 
 fun getTrayIcon(): BufferedImage {
