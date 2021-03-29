dfa4a44829f29998a12c3a159be2dac01244235f
hyia <zs360@qq.com>
2021-03-29 17:38:00 +0800

    update

--- src/main/kotlin/ilio/translation/main.kt
+++ src/main/kotlin/ilio/translation/main.kt
@@ -2,40 +2,35 @@ package ilio.translation
 
 import androidx.compose.desktop.AppManager
 import androidx.compose.desktop.AppWindow
-import androidx.compose.desktop.WindowEvents
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults.buttonColors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.window.MenuItem
 import androidx.compose.ui.window.Notifier
 import androidx.compose.ui.window.Tray
 import java.awt.Window
 import java.awt.image.BufferedImage
-import javax.swing.JFrame
 import kotlin.system.exitProcess
 
 private val init_block: Unit = run {
     // It will cause no auto focus while debugging
-    System.setProperty("apple.awt.UIElement", "true")
+    if (System.getProperty("debugging") != "true") {
+        System.setProperty("apple.awt.UIElement", "true")
+    }
 }
 
 val notifier: Notifier = Notifier()
 
 fun main() {
-    val frame = JFrame()
-    frame.setSize(300, 450)
-//    frame.isVisible = false
     Tray().apply {
         icon(getTrayIcon())
         menu(
             MenuItem("Open", {
-                frame.isVisible = true
-                frame.topMe()
-//                ctx().show()
+                ctx().showMe()
             }),
             MenuItem("Exit", { exitProcess(0) })
         )
@@ -48,17 +43,14 @@ fun ctx(): AppWindow = Context("main") {
         size = IntSize(700, 450),
         resizable = false,
         undecorated = false,
-        events = WindowEvents(
-            onClose = {
-                print("abc")
-            }
-        )
-    ).apply {
-        window.apply {
-////            isAutoRequestFocus = true
-//            isFocusable = true
+    ).apply { window.apply {
+        defaultCloseOperation = 0
+
+        keyboard.setShortcut(Key.Escape) {
+            // TODO: Confirm to save or close
+            ctx().hideMe()
         }
-    }
+    } }
 
     context.show {
         MaterialTheme {
@@ -89,15 +81,13 @@ fun Window.topMe() {
     isAlwaysOnTop = false
 }
 
-fun AppWindow.show() {
+fun AppWindow.showMe() {
     this.window.apply {
         isVisible = true
         topMe()
-        val focusRequester = FocusRequester()
-        focusRequester.requestFocus()
     }
 }
 
-fun AppWindow.hide() {
+fun AppWindow.hideMe() {
     this.window.isVisible = false
 }
